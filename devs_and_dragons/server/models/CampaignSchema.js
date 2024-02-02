fe9859e65942ab272b687bc914e5ebed6e4ebd64
Andrew Allison <andrewallison5211@gmail.com>
2024-02-01 23:46:00 -0700

    data model update

--- devs_and_dragons/server/models/CampaignSchema.js
+++ devs_and_dragons/server/models/CampaignSchema.js
@@ -2,7 +2,7 @@ import { Schema } from "mongoose";
 
 export const CampaignSchema = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
-  coverImg: { type: String, minLength: 3, maxLength: 400 },
+  coverImg: { type: String, minLength: 3, maxLength: 400, default: "https://t4.ftcdn.net/jpg/01/71/83/83/360_F_171838349_PPKsHSTz1xZxfrUBKJxXNHnf6MLInNm2.jpg" },
   desc: { type: String, required: true, minLength: 10, maxLength: 2500 },
   creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
   isPrivate: { type: Boolean, default: false },
