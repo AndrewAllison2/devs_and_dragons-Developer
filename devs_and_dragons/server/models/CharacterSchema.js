20af9e9a482c286c7777a71b721d40da5bdbe505
Andrew Allison <andrewallison5211@gmail.com>
2024-02-03 14:19:11 -0700

    schema

--- devs_and_dragons/server/models/CharacterSchema.js
+++ devs_and_dragons/server/models/CharacterSchema.js
@@ -1,6 +1,7 @@
 import { Schema } from "mongoose";
 
 export const CharacterSchema = new Schema({
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
   coverImg: { type: String, minLength: 3, maxLength: 400 },
   race: { type: String, enum: ['human', 'elf', 'half-elf', 'dwarf', 'gnome', 'halfling', 'tiefling', 'half-orc', 'dragonborn'], lowercase: true, required: true },
@@ -8,5 +9,11 @@ export const CharacterSchema = new Schema({
   level: { type: Number, required: true },
   healthPoints: { type: Number, required: true },
   armorClass: { type: Number, required: true },
+  strength: { type: Number, required: true, },
+  dexterity: { type: Number, required: true, },
+  constitution: { type: Number, required: true, },
+  intelligence: { type: Number, required: true, },
+  wisdom: { type: Number, required: true, },
+  charisma: { type: Number, required: true, }
 
 })
