7d9747689dd4b095b5710b8fc5d7cc8145866667
Andrew Allison <andrewallison5211@gmail.com>
2024-02-05 11:03:59 -0700

    test

--- devs_and_dragons.client/src/env.js
+++ devs_and_dragons.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://api.devs-and-dragons.onrender.com' : ''
 export const useSockets = false
 export const domain = 'dev-74troqf32atk1sf4.us.auth0.com'
 export const clientId = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
