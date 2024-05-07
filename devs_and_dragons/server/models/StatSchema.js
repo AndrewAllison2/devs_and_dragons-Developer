afd61618af95325be58a9bfc37e05f9797de2270
Andrew Allison <andrewallison5211@gmail.com>
2024-05-07 12:41:34 -0600

    getting stats

--- devs_and_dragons/server/models/StatSchema.js
+++ devs_and_dragons/server/models/StatSchema.js
@@ -4,6 +4,7 @@ export const StatSchema = new Schema({
   creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
   coverImg: { type: String, minLength: 3, maxLength: 400, default: "https://www.vecteezy.com/vector-art/550980-user-icon-vector" },
+  description: { type: String, maxLength: 2500 },
   race: { type: String, enum: ['Human', 'Elf', 'Half-Elf', 'Dwarf', 'Gnome', 'Halfling', 'Tiefling', 'Half-Orc', 'Dragonborn'], required: true },
   class: { type: String, enum: ['Barbarian', 'Paladin', 'Druid', 'Wizard', 'Fighter', 'Ranger', 'Rogue', 'Bard', 'Sorcerer', 'Warlock', 'Artificer'], required: true },
   level: { type: Number, required: true, default: 1 },
