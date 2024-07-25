0ad17a1fe9f4815d67c3d5df1e1460e097bce78f
Andrew Allison <andrewallison5211@gmail.com>
2024-07-25 13:04:14 -0600

    model done

--- devs_and_dragons.client/src/models/Character.js
+++ devs_and_dragons.client/src/models/Character.js
@@ -4,31 +4,20 @@ export class Character {
     this.creatorId = data.creatorId
     this.name = data.name
     this.coverImg = data.coverImg
-
+    this.race = data.race
+    this.class = data.class
+    this.level = data.level
+    this.healthPoints = data.healthPoints
+    this.armorClass = data.armorClass
+    this.experiencePoints = data.experiencePoints
+    this.strength = data.strength
+    this.dexterity = data.dexterity
+    this.constitution = data.constitution
+    this.intelligence = data.intelligence
+    this.wisdom = data.wisdom
+    this.charisma = data.charisma
+    this.campaignId = data.campaignId
+    this.createdAt = data.createdAt
+    this.updatedAt = data.updatedAt
   }
-}
-
-
-let c = {
-  "creatorId": "64f00a8ad19db0edfecf21e7",
-  "name": "Soup",
-  "coverImg": "https://www.vecteezy.com/vector-art/550980-user-icon-vector",
-  "race": "Half-Elf",
-  "class": "Barbarian",
-  "level": 1,
-  "healthPoints": 100,
-  "armorClass": 22,
-  "experiencePoints": 0,
-  "strength": 18,
-  "dexterity": 8,
-  "constitution": 20,
-  "intelligence": 4,
-  "wisdom": 4,
-  "charisma": 10,
-  "campaignId": "65df732680300c6864d4e044",
-  "_id": "66a29feafd4aa87f79e7cafd",
-  "createdAt": "2024-07-25T18:56:42.087Z",
-  "updatedAt": "2024-07-25T18:56:42.087Z",
-  "__v": 0,
-  "id": "66a29feafd4aa87f79e7cafd"
 }
