26052bfebb0b6b613a2e97ae913251b32e7c9602
Andrew Allison <andrewallison5211@gmail.com>
2024-10-24 13:58:46 -0700

    prod change

--- devs_and_dragons.client/src/env.js
+++ devs_and_dragons.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-// export const baseURL = 'https://devs-and-dragons-developer.onrender.com/'
-export const baseURL = 'http://127.0.0.1:3000'
+export const baseURL = 'https://devs-and-dragons-developer.onrender.com/'
+// export const baseURL = 'http://127.0.0.1:3000'
 export const useSockets = false
 export const domain = 'dev-74troqf32atk1sf4.us.auth0.com'
 export const clientId = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
