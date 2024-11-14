801d4318903942fe8aa7958e85a40b963fbb26d9
Andrew Allison <andrewallison5211@gmail.com>
2024-11-13 21:25:51 -0800

    account page stuff

--- devs_and_dragons.client/src/components/EntityOptionsComponent.vue
+++ devs_and_dragons.client/src/components/EntityOptionsComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    Options
+    <div class="text-center">
+      <button class="btn btn-outline-warning text-white">EDIT</button>
+    </div>
+    <div class="text-center mt-3">
+      <button @click="removeEntity()" class="btn btn-outline-danger text-white">DELETE</button>
+    </div>
   </div>
 </template>
 
